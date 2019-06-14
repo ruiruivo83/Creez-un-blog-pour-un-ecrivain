8bf702d19ffa53ba7fed7fb02c31f5ee06afa491
ruiruivo <ruivo.rui@gmail.com>
2019-06-14 11:18:40 +0200

    new commit

--- build01/test00 - Main/index_controller.php
+++ build01/test00 - Main/index_controller.php
@@ -1,29 +1,23 @@
 <?php
 
-// 1
-// Premier appel - Charge en memoire le fichier index avec la vue principale
+// 1 - DECLARE et Charge en memoire la vue principale et tout les fichiers necessaires au remplissage de la page.
 $view = file_get_contents("index_vue.html");
-
-// 2
-// IMPORT des fichiers secondaires necessaires pour toute la page a reproduire
 $article_view = file_get_contents("index_article.html");
 
-// 2.1 PREPARE LE CODE POUR LA VUE
-// String Code pour représenter le code final.
-$bloc_article = "";
-for ($i = 0; $i<10; $i++) {
-    $bloc_article .= $article_view; // comme +=
+// 2.1 - String Code pour représenter la liste des articles a présenter.
+$bloc_article = ""; // String vide a remplir
+for ($i = 0; $i<10; $i++) { // boucle pour remplir la string de code necessaire a présenter
+    $bloc_article .= $article_view; // remplissage a chaque boucle
 }
+
+// ASSIGNE -> REMPLACE {LIST_ARTICLE} PAR LE CODE $bloc_article dans la $view
 $view = str_replace("{LIST_ARTICLE}", $bloc_article, $view);
 
-// 2.2 Done d'autres valeurs a d'aurtes variables necessaires
-// NEW ASSIGN CURRENT DATE TO VARIABLE DATE
+// NOUVELLE STRING POUR LA DATE
 $date_article = "10/06/2019";
 
-
-
-// 3 
-// ORDONE LE REMPLACEMENT DANS LE CODE HTML DE LA STRING {DATE_ARTICLE} PAR TOUTES LES STRINGS NECESSAIRES
+// REMPLACE DATE_ARTICLE par $date_article dans la $view
 $view = str_replace("{DATE_ARTICLE}", $date_article, $view);
 
+// AFICHE LA VUE FINAL AVEC LE CODE
 echo $view;
